9df535c67fdcf8761a9e9d8d97a870d00e3c96cf
Brad <bradktracy@gmail.com>
2018-11-18 08:24:31 -0500

    updated UI and index.js

--- Clockwork.Web/Scripts/index.js
+++ Clockwork.Web/Scripts/index.js
@@ -28,34 +28,39 @@
             GetCurrentTime: function () {
                 var url = "http://localhost:56253/api/currenttime";
                 var _this = this;
-                this.RequestData(url).then(function (resp) {
+                this.MakeRequest(url).then(function (resp) {
                     _this.currentTimeRequest = JSON.parse(resp);
                 });
             },
             GetAllData: function () {
                 var url = "http://localhost:56253/api/clockworkdata";
                 var _this = this;
-                this.RequestData(url).then(function (resp) {
+                this.MakeRequest(url).then(function (resp) {
                     _this.allDataEntries = JSON.parse(resp);
                 });
             },
             ToggleDisplayData: function () {
                 this.isDisplayLocalized = !this.isDisplayLocalized;
                 if (this.isDisplayLocalized) {
-                    this.RecordTimeZone("http://localhost:56253/api/timezone", this.targetZone);
-                    this.UpdateToLocalizedTime();
+                    var url = "http://localhost:56253/api/timezone" + "?selectedTimeZone=" + this.targetZone;
+                    this.MakeRequest(url);
+                    
+                    this.allDataEntriesLocalized = [];
+                    this.allDataEntriesLocalized = _.map(this.UpdateToLocalizedTime(this.allDataEntries), _.clone);
                 }
             },
-            UpdateToLocalizedTime: function () {
-                this.allDataEntriesLocalized = [];
-                var clone = _.map(this.allDataEntries, _.clone);
+            UpdateToLocalizedTime: function (dataArray) {
+                
+                var clone = _.map(dataArray, _.clone);
                 for (i = 0; i < clone.length; i++) {
                     var entry = clone[i];
                     entry.time = moment(entry.time).tz(this.targetZone).format('MMMM Do YYYY, h:mm:ss a');
-                    this.allDataEntriesLocalized.push(entry);
+                    //this.allDataEntriesLocalized.push(entry);
                 };
+
+                return clone;
             },
-            RequestData: function (url) {
+            MakeRequest: function (url) {
                 return Q.Promise(function (resolve, reject, notify) {
                     var xhttp = new XMLHttpRequest();
 
@@ -79,24 +84,14 @@
                     }
 
                     function onprogress(event) {
+                        console.log(event);
                         notify(event.loaded / event.total);
                     }
                 });
-            },
-            RecordTimeZone: function (url, timezone) {
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        console.log("timezone save successful");
-                    }
-                };
-                xhttp.open("GET", url + "?selectedTimeZone=" + timezone, true);
-                xhttp.setRequestHeader("Content-type", "application/json");
-                xhttp.send();
             },
         }
     });
 
     app.GetAllData();
-    
+
 };
